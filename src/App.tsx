81693a3d5b1b540c139751b018d55869d1015fc3
sanskardudhe09 <sanskardudhe2018@gmail.com>
2025-10-05 13:49:46 +0530

    ADD regex testing for city name

--- src/App.tsx
+++ src/App.tsx
@@ -39,6 +39,9 @@ const App:React.FC = () => {
       response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}&units=metric`);
       const weatherRes = await response.json();
       if(!response.ok) throw new Error(weatherRes.message || 'City Not Found');
+      if (/^[0-9]+$/.test(weatherRes.name)) {
+        throw new Error("Invalid city name.");
+      }
       response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${API_KEY}&units=metric`);
       const forecastRes = await response.json();
       setWeatherData(weatherRes);
